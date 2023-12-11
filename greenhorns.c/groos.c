b9f522c5a6a18eca8ec8f27de8d0baddc2954c2b
YASHrnw <140162246+YASHrnw@users.noreply.github.com>
2023-12-11 12:50:27 +0530

    Update groos.c

--- greenhorns.c/groos.c
+++ greenhorns.c/groos.c
@@ -3,18 +3,23 @@
 int main()
 {
 
-    int y, hra, da, ta;
+     int salary;
+    float hra, da, ta, grossalary;
 
-    printf("enter your slary");
-    scanf("%d", &y);
+    printf("enter salary:");
+    scanf("%d", &salary);
 
-    printf("\nhra %d", hra = y * 10 / 100);
-    printf("\nda %d", da = y * 5 / 100);
-    printf("\nta %d", ta = y * 8 / 100);
+    hra = salary * 10 / 100;
+    da = salary * 5 / 100;
+    ta = salary * 8 / 100;
 
-    printf("total sl %d", y+ hra + da + ta);
+    grossalary = salary + hra + ta + da;
+    printf("\nenter your salary %d", salary);
+    printf("\n hra value %.2f", hra);
+    printf("\n da value %.2f", da);
+    printf("\n ta value %.2f", ta);
 
-   
+    printf("\ngrossl %.2f", grossalary);
 
     return 0;
 }
