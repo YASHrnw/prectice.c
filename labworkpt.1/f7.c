5491da50770f012b3a3ff6d27176de0098fb5ef4
YASHrnw <140162246+YASHrnw@users.noreply.github.com>
2023-12-25 10:36:00 +0530

    Add files via upload

---
+++ labworkpt.1/f7.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(){
+    
+    char i,j;
+
+    for(i='a';i<='e';i++){ 
+        for(j=i;j>='a';j--){
+            printf(" %c",j);
+        }
+        printf("\n");
+
+    }
+
+
+    return 0;
+}
+
