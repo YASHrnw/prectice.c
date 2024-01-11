aadeb642263347ed50b060d61e24deead3be9c57
YASHrnw <140162246+YASHrnw@users.noreply.github.com>
2024-01-11 18:29:04 +0530

    Add files via upload

---
+++ infini.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+
+
+int addition();// function declarations
+int minus();
+int multiply();
+int divide();
+
+int main()
+{
+
+    // for (op = 0; op >= 7; op++)
+    // {
+    //     /* code */
+    // }
+
+    int u1;
+
+    do
+    {
+
+        printf("\n 1 Addition  2 minus  3 Multiplication  4 Division  Please chose ");
+
+        scanf("%d", &u1);
+
+        switch (u1)
+        {
+        case 1:
+            addition();
+            break;
+
+        case 2:
+            minus();
+            break;
+
+        case 3:
+            multiply();
+            break;
+
+        case 4:
+            divide();
+            break;
+
+        default:
+            printf(" Something is wrong!! ");
+            break;
+        }
+
+    } while (u1 != 7);
+
+
+    
+
+    return 0;
+}
+
+// function definition
+int addition()
+{
+
+    int a, b;
+    printf("enter value a&b:");
+    scanf("\n %d %d", &a, &b);
+    printf("two addition a+b %d", a + b);
+}
+
+int minus()
+{
+    int a, b;
+    printf("enter value a&b: ");
+    scanf("%d  %d", &a, &b);
+    printf("minus %d", a - b);
+}
+
+int multiply()
+{
+    int a, b;
+    printf(" enter valu a&b: ");
+    scanf(" %d %d", &a, &b);
+    printf(" The multiply of %d",a * b);
+}
+
+int divide()
+{
+    int a, b;
+    printf(" enterd value a&b: ");
+    scanf("%d %d", &a, &b);
+    printf("the value divide %d", a / b);
+}
