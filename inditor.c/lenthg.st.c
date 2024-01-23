997da89a6432576ecfb3e6bca278fb996a250156
YASHrnw <140162246+YASHrnw@users.noreply.github.com>
2024-01-23 15:14:00 +0530

    Add files via upload

---
+++ inditor.c/lenthg.st.c
@@ -0,0 +1,23 @@
+//  find length of string using pointer
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+
+    char str[50], *ptr;
+    int count = 0;
+    printf("enetr name:");
+    gets(str);
+    printf("name is:");
+    puts(str);
+    ptr = str;
+    while (*ptr != '\0')
+    {
+        count++;
+        ptr++;
+    }
+    printf(" length of the string is: %d", count);
+
+    return 0;
+}
